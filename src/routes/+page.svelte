fcff6adc336014463a0b4b2a858f38b456ba9966
poraif <peadar.oraif@gmail.com>
2025-02-27 15:52:10 +0100

    added config for setting sveltekit to spa mode, and to connect with backend server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="bg-green-900">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: "index.html",
+			precompress: false,
+			strict: true
+		}
+		)
 	}
 };
 
