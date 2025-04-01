f562b78b2f65f7c97945420f2a11949b1c6d871b
poraif <peadar.oraif@gmail.com>
2025-04-01 13:18:55 +0200

    testing vercel adapter instead

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  kit: {
-    adapter: adapter({
-      fallback: 'index.html' 
-    })
-  }
+	kit: {
+		adapter: adapter({
+		})
+	}
 };
