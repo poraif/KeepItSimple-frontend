acd64f579380bb34b39e7a12db045daec3a80ae2
poraif <peadar.oraif@gmail.com>
2025-04-01 13:02:42 +0200

    config changes for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: "index.html",
-			precompress: false,
-			strict: true
-		}
-		)
+		adapter: adapter({})
 	}
 };
-
-export default config;
